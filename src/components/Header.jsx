0575a257c67d6b16ea2507c74ceedcee0be84ed4
IL <53288945+irlfslab@users.noreply.github.com>
2025-02-12 23:24:38 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,7 @@ const Header = ({children, theme, setTheme}) => {
             <span className="themeSelector">
                 <span onClick={() => setTheme("light")} className={ theme === "light" ? "light activeTheme" : "light"}></span>
                 <span onClick={() => setTheme("medium")} className={ theme === "medium" ? "medium activeTheme" : "medium"}></span>
-                <span onClick={() => setTheme("dark")} className={ theme === "dark" ? "dark activeTheme" : "dark"}></span>
-                <span onClick={() => setTheme("gOne")} className={ theme === "gOne" ? "gOne activeTheme" : "gOne"}></span>
-                <span onClick={() => setTheme("gTwo")} className={ theme === "gTwo" ? "gTwo activeTheme" : "gTwo"}></span>
-                <span onClick={() => setTheme("gThree")} className={ theme === "gThree" ? "gThree activeTheme" : "gThree"}></span>
+                <span onClick={() => setTheme("blue")} className={ theme === "blue" ? "dark activeTheme" : "blue"}></span>
             </span>
         </header>
     )
