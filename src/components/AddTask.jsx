96521d948816227f8da7cc7733d9618de4e716b3
IL <53288945+irlfslab@users.noreply.github.com>
2025-02-15 00:32:57 -0500

    Update AddTask.jsx

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,29 @@
-const AddTask = ({handleSubmit, editid, task, setTask}) => {
+export const AddTask = ({taskList, setTaskList, task, setTask}) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (task.id) {
+            const date = new Date();
+            const updateTask = taskList.map((todo)) => (
+                todo.id === task.id ? {id: task.id, name: e.target.task.value, time: `$(date.toLocaleTimeString()} $(date.toLocaleDateString()}`} : todo
+                ));
+            setTasklist(updatedTasklist);
+        } else {
+            const date = new Date();
+            const newTask = {
+                id: date.getTime(),
+                name: e.target.task.value,
+                time: `$(date.toLocaleTimeString()} $(date.toLocaleDateString()}`
+            }
+            setTasklist([...taskList, newTask]);
+            e.target.task.value ="";
+        }
     return (
         <section className='addTask'>
           <form onSubmit={handleSubmit}>
-            <input type="text" name="task" value={task} autoComplete="off" placeholder="add task" maxLength="25" onChange={(e) => setTask(e.target.value)}/>
+            <input type="text" name="task" value={task.name} autoComplete="off" placeholder="add task" maxLength="25" onChange={(e) => setTask(e.target.value)}/>
             <button type="submit">{ editid ? "Update" : "Add"}</button>
           </form>
         </section>
     );
 }
-
-export default AddTask;
