384944dc55aa558104a4b0565dc337a2ef031985
IL <53288945+irlfslab@users.noreply.github.com>
2025-02-23 23:44:41 -0500

    Update AddTask.jsx

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,29 +1,31 @@
 export const AddTask = ({taskList, setTaskList, task, setTask}) => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+      e.preventDefault();
 
-        if (task.id) {
-            const date = new Date();
-            const updateTask = taskList.map((todo)) => (
-                todo.id === task.id ? {id: task.id, name: e.target.task.value, time: `$(date.toLocaleTimeString()} $(date.toLocaleDateString()}`} : todo
-                ));
-            setTasklist(updatedTasklist);
-        } else {
-            const date = new Date();
-            const newTask = {
-                id: date.getTime(),
-                name: e.target.task.value,
-                time: `$(date.toLocaleTimeString()} $(date.toLocaleDateString()}`
-            }
-            setTasklist([...taskList, newTask]);
-            e.target.task.value ="";
-        }
-    return (
-        <section className='addTask'>
-          <form onSubmit={handleSubmit}>
-            <input type="text" name="task" value={task.name ||""} autoComplete="off" placeholder="add task" maxLength="25" onChange={(e) => setTask(...task, name: e.target.value)}/>
-            <button type="submit">{ editid ? "Update" : "Add"}</button>
-          </form>
-        </section>
-    )
+      if (task.id) {
+          const date = new Date();
+          const updateTask = taskList.map((todo) => (
+              todo.id === task.id ? {id: task.id, name: e.target.task.value, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
+          ));
+          setTasklist(updatedTasklist);
+      } else {
+          const date = new Date();
+          const newTask = {
+              id: date.getTime(),
+              name: e.target.task.value,
+              time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+          }
+          setTasklist([...taskList, newTask]);
+          e.target.task.value ="";
+      }
+  }
+  return (
+      <section className='addTask'>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="task" value={task.name ||""} autoComplete="off" placeholder="add task" maxLength="25" onChange={e => setTask({...task, name: e.target.value})}/>
+          <button type="submit">{ task.id ? "Update" : "Add"}</button>
+        </form>
+      </section>
+  )
 }
+export default AddTask;
